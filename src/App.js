b0f38aa2890be523456c2e37f3322f7b56fd2af7
somang1206 <dlthakd1206@naver.com>
2022-11-03 16:08:25 +0900

    netflix_app_react

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 
+import requests from './api/request';
 import './App.css';
-import Banner from "./components/Banner"
+import Banner from "./components/Banner";
 import Nav from "./components/Nav";
+import Row from "./components/Row";
+
 
 
 function App() {
@@ -9,6 +12,19 @@ function App() {
     <div className="App">
       <Nav />
       <Banner />
+
+      <Row 
+        title="NETFLIX ORIGINALS"
+        id="NO"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+
+      <Row title="Trending Now" id="TN" fetchUrl={requests.fetchTrending}/>
+      <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated}/>
+      <Row title="Action Movies" id="AM" fetchUrl={requests.fetchActionMovies}/>
+      <Row title="Comedy Movies" id="CM" fetchUrl={requests.fetchComedyMovies}/>
+
     </div>
   );
 }

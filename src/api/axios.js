bc3f2c6d9732a10c3080433627c1ef8f237eb6f0
somang1206 <dlthakd1206@naver.com>
2023-02-16 17:47:06 +0900

    넷플릭스 앱 만들기 완성

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
     baseURL: "https://api.themoviedb.org/3",
     params:{
-        api_key:"d4b4f1e899eac6bca01ee9ddb15dc1f1",
+        api_key: process.env.REACT_APP_MOVIE_DB_API_KEY,
         language:"ko-KR",
     }
 });

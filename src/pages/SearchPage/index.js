09134e24281b0299d7163b0b76b281fd6947b498
somang1206 <dlthakd1206@naver.com>
2023-02-16 12:35:55 +0900

    debounce hook

--- src/pages/SearchPage/index.js
+++ src/pages/SearchPage/index.js
@@ -1,72 +1,77 @@
 import axios from '../../api/axios';
 import React, { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from "react-router-dom";
 import "./SearchPage.css";
+import { useDebounce } from '../../hooks/useDebounce';
 
 export default function SearchPage() {
-  
-
-
-
+    const navigate = useNavigate();
     const [searchResults, setSearchResults] = useState([]);
     const useQuery = () => {
         return new URLSearchParams(useLocation().search);
-    }
+    };
 
     let query = useQuery();
     const searchTerm = query.get("q")
+    const debouncedSearchTerm = useDebounce(searchTerm, 500)
     console.log('searchTerm', searchTerm);
     
 
     useEffect(() => {
-        if(searchTerm) {
-            fetchSearchMovie(searchTerm);
+        if(debouncedSearchTerm) {
+            fetchSearchMovie(debouncedSearchTerm);
         }
-    }, [searchTerm]);
+    }, [debouncedSearchTerm]);
 
 
     const fetchSearchMovie = async (searchTerm) =>{
+        console.log("searchTerm", searchTerm);
         try {
             const request = await axios.get(
                 `/search/multi?include_adult=false&query=${searchTerm}`
-            )
+            );
             console.log(request);
             setSearchResults(request.data.results);
-        } catch(error){
+        } catch (error) {
             console.log("error", error);
         }
-    }
+    };
    
     const renderSearchResults = () => {
         return searchResults.length > 0 ? (
-            <section className='search-container'>
-                {searchResults.map((movie) => {
-                    if(movie.backdrop_path !== null && movie.media_type !== "person") {
-                        const movieImageUrl =
-                        "https://image.tmdb.org/t/p/w500" + movie.backdrop_path
-                        return(
-                            <div className='movie'>
-                                <div className='movie__column-poster'>
-                                    <img 
-                                    src={movieImageUrl}
-                                    alt="movie"
-                                    className='movie__poster'
-                                    />
-                                </div>
-                            </div>
-                        )
-                    }
-                })}
-            </section>
+          <section className="search-container">
+            {searchResults.map((movie) => {
+              if (movie.backdrop_path !== null && movie.media_type !== "person") {
+                const movieImageUrl =
+                  "https://image.tmdb.org/t/p/w500" + movie.backdrop_path;
+                return (
+                  <div className="movie" key={movie.id}>
+                    <div
+                      onClick={() => navigate(`/${movie.id}`)}
+                      className="movie__column-poster"
+                    >
+                      <img
+                        src={movieImageUrl}
+                        alt="movie"
+                        className="movie__poster"
+                      />
+                    </div>
+                  </div>
+                );
+              }
+            })}
+          </section>
         ) : (
-            <section className='no-results'>
-                <div className='no-results__text'>
-                    <p>
-                        찾고자하는 검색어 "{searchTerm}에 맞는 영화가 없습니다."
-                    </p>
-                </div>
-            </section>
-        )
+          <section className="no-results">
+            <div className="no-results__text">
+              <p>
+                찾고자하는 검색어"{debouncedSearchTerm}"에 맞는 영화가 없습니다.
+              </p>
+            </div>
+          </section>
+        );
+      };
+    
+      return renderSearchResults();
     }
-return renderSearchResults();
-}
+    
